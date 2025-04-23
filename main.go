10348dd7cde4b2ab6a977c3706eb05044f40c612
TepidMilk <ivorykingrey@gmail.com>
2025-04-22 21:33:06 -0500

    Update main
    main now creates pokeapi client and config to
    start REPL

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"time"
+
+	"github.com/tepidmilk/pokedex/internal/pokeapi"
 )
 
 func main() {
-	startRepl()
-}
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &Config{
+		pokeapiClient: pokeClient,
+	}
 
+	startRepl(cfg)
+}
