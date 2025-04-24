95cd25a9468b226008363d9931662a27deaa0896
TepidMilk <ivorykingrey@gmail.com>
2025-04-23 22:32:56 -0500

    Update location_list to use cache

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -2,13 +2,23 @@ package pokeapi
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 )
 
 func (c *Client) ListLocations(pageURL *string) (Locations, error) {
 	url := baseURL + "/location-area"
+	var locationsResp Locations
 	if pageURL != nil {
 		url = *pageURL
+	}
+	dat, ok := c.cache.Get(url)
+	if ok {
+		err := json.Unmarshal(dat, &locationsResp)
+		if err != nil {
+			return Locations{}, err
+		}
+		return locationsResp, nil
 	}
 
 	req, err := http.NewRequest("GET", url, nil)
@@ -20,9 +30,13 @@ func (c *Client) ListLocations(pageURL *string) (Locations, error) {
 	if err != nil {
 		return Locations{}, err
 	}
-
 	defer res.Body.Close()
-	var locationsResp Locations
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return Locations{}, err
+	}
+	c.cache.Add(url, body)
+
 	decoder := json.NewDecoder(res.Body)
 	if err = decoder.Decode(&locationsResp); err != nil {
 		return Locations{}, err
