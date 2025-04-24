3930340b0a130ea4fe9e239319c2aafa84b59b73
TepidMilk <ivorykingrey@gmail.com>
2025-04-24 17:41:02 -0500

    Fix print statement to look cleaner

--- command_explore.go
+++ command_explore.go
@@ -16,10 +16,10 @@ func commandExplore(location string, cfg *Config) error {
 
 	cfg.current = &location
 
-	fmt.Printf("\nExploring %s...\n", location)
-	fmt.Print("Found Pokemon:\n\n")
+	fmt.Printf("Exploring %s...\n", location)
+	fmt.Print("Found Pokemon:\n")
 	for _, pokemonEncounter := range exploreResp.PokemonEncounters {
-		fmt.Println(pokemonEncounter.Pokemon.Name)
+		fmt.Println(" - ", pokemonEncounter.Pokemon.Name)
 	}
 
 	return nil
