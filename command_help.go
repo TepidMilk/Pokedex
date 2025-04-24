b40859f5c06ef696cbd1f6c8e5e5c132a58bf741
TepidMilk <ivorykingrey@gmail.com>
2025-04-23 22:43:42 -0500

    Fix redundant newline

--- command_help.go
+++ command_help.go
@@ -6,7 +6,7 @@ import (
 
 func commandHelp(cfg *Config) error {
 	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:\n")
+	fmt.Println("Usage:")
 	for _, value := range getCommands() {
 		fmt.Printf("%s: %s\n", value.name, value.description)
 	}
