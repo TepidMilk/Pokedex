a412c61ea371bb3f14fdf59855efa738ce67bb82
TepidMilk <ivorykingrey@gmail.com>
2025-04-25 12:37:11 -0500

    Add inspect command

---
+++ command_inspect.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func commandInspect(pokemon string, cfg *Config) error {
+	if _, ok := cfg.Pokedex[pokemon]; !ok {
+		return errors.New("you have not caught that pokemon")
+	}
+	fmt.Printf("Name: %s\n", cfg.Pokedex[pokemon].Name)
+	fmt.Printf("Height: %d\n", cfg.Pokedex[pokemon].Height)
+	fmt.Printf("Weight: %d\n", cfg.Pokedex[pokemon].Weight)
+	fmt.Println("Stats:")
+	for _, stat := range cfg.Pokedex[pokemon].Stats {
+		fmt.Printf("  -%s: %d\n", stat.Stat.Name, stat.BaseStat)
+	}
+	fmt.Println("Types:")
+	for _, t := range cfg.Pokedex[pokemon].Types {
+		fmt.Printf("  -%s\n", t.Type.Name)
+	}
+	return nil
+}
