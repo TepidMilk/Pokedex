0542b5c88e1ee2545dcef7ee21ded2809a043047
TepidMilk <ivorykingrey@gmail.com>
2025-04-25 12:52:34 -0500

    Update cleaner print statements

--- command_inspect.go
+++ command_inspect.go
@@ -14,11 +14,11 @@ func commandInspect(pokemon string, cfg *Config) error {
 	fmt.Printf("Weight: %d\n", cfg.Pokedex[pokemon].Weight)
 	fmt.Println("Stats:")
 	for _, stat := range cfg.Pokedex[pokemon].Stats {
-		fmt.Printf("  -%s: %d\n", stat.Stat.Name, stat.BaseStat)
+		fmt.Printf(" - %s: %d\n", stat.Stat.Name, stat.BaseStat)
 	}
 	fmt.Println("Types:")
 	for _, t := range cfg.Pokedex[pokemon].Types {
-		fmt.Printf("  -%s\n", t.Type.Name)
+		fmt.Println(" -", t.Type.Name)
 	}
 	return nil
 }
